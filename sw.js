717aa2319b673bf9f2a9627945e9e5a5d61a3abc
Tribi1711 <hugo.tribaldos@carmelitaselda.com>
2025-05-29 17:47:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,24 @@
-const CACHE_NAME = 'avtpegar-v1';
+const CACHE_NAME = 'pega-aitor-v1';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/icono.json', // or icono.png if you change it
-  '/manifest.json',
   '/styles.css',
-  '/app.js'
+  '/script.js',
+  '/images.jpeg',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Cache abierto');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then((response) => response || fetch(event.request))
   );
 });
