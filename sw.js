f3b9c94006845301382ac1d1a51d2e05917a6e4e
Tribi1711 <hugo.tribaldos@carmelitaselda.com>
2025-05-29 17:34:27 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'avtpegar-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icono.json', // or icono.png if you change it
+  '/manifest.json',
+  '/styles.css',
+  '/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache abierto');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
